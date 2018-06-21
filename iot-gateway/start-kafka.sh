0d2c267b56bed870dd6f5593b313e6fc6cca4acb
Clement Escoffier <clement.escoffier@gmail.com>
2018-06-21 13:30:06 +0200

    change how kafka is started

--- iot-gateway/start-kafka.sh
+++ iot-gateway/start-kafka.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-docker run -p 2181:2181 -p 9092:9092 --env ADVERTISED_HOST=localhost --env ADVERTISED_PORT=9092 teivah/kafka
+docker run -d --name zookeeper -p 2181:2181 confluent/zookeeper
+docker run -d --name kafka -p 9092:9092 --link zookeeper:zookeeper \
+    --env KAFKA_ADVERTISED_HOST_NAME=localhost confluent/kafka
+
